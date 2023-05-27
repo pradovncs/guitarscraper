e075b9fd2f63c329d0c263210190fb674871cf41
pradovncs <pradovncs@gmail.com>
2023-05-27 17:38:55 -0300

    Add: Docstrings to 'save_csv'.

--- csv_utils.py
+++ csv_utils.py
@@ -1,6 +1,16 @@
 import csv
 
 def save_csv(results: list[dict], store: str):
+    """
+    Saves a list of dictionaries to a CSV file.
+
+    Args:
+        results (list[dict]): The list of dictionaries to be saved as CSV.
+        store (str): The name of the store.
+
+    Returns:
+        None
+    """
     keys = results[0].keys()
 
     with open(f'{store}_products.csv', 'w', newline='') as f:
